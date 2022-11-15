77a562ab308bfa4e93caa6e30bb0b3249ce50622
mdsung <mdskylover@gmail.com>
2022-11-15 09:54:47 +0900

    Change raw data - the only fatal data in this raw data

--- src/download_raw_data.sh
+++ src/download_raw_data.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # Download raw data from the web
-## Study: An immune-cell signature of bacterial sepsis (Patient PBMCs)
-## https://singlecell.broadinstitute.org/single_cell/study/SCP548/an-immune-cell-signature-of-bacterial-sepsis-patient-pbmcs#/
-## using bulk download - need the authentication code to download. 
-## the authentication code is valid for 30 minutes.
-## When click the buld download, you can check download curl commands in the browser console.
+## Study: Dynamic changes in human single cell transcriptional signatures during fatal sepsis
+## GSE167363
 
-curl -k "https://singlecell.broadinstitute.org/single_cell/api/v1/bulk_download/generate_curl_config?accessions=SCP548&auth_code=eW7JLBAD&directory=all&context=study"  -o cfg.txt; curl -K cfg.txt && rm cfg.txt
+wget -e robots=off -U mozilla 'https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE167363&format=file' -O data/raw/GSE167363_RAW.tar
+tar xvf data/raw/GSE167363_RAW.tar -C data/raw/
+rm data/raw/GSE167363_RAW.tar
 
-mv SCP548/ data/raw/
