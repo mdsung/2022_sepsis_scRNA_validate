f49fc1d6558109369453804d4201078acffae314
mdsung <mdskylover@gmail.com>
2022-11-15 10:08:24 +0900

    Add patient metadata file creator

--- src/download_raw_data.sh
+++ src/download_raw_data.sh
@@ -8,3 +8,17 @@ wget -e robots=off -U mozilla 'https://www.ncbi.nlm.nih.gov/geo/download/?acc=GS
 tar xvf data/raw/GSE167363_RAW.tar -C data/raw/
 rm data/raw/GSE167363_RAW.tar
 
+## Create Patient metadata file
+echo -e "GSM5102900	HC1" >> data/raw/metadata.txt
+echo -e "GSM5102901	HC2" >> data/raw/metadata.txt
+echo -e "GSM5102902	NS LS T0" >> data/raw/metadata.txt
+echo -e "GSM5102903	NS LS T6" >> data/raw/metadata.txt
+echo -e "GSM5102904	S1 T0" >> data/raw/metadata.txt
+echo -e "GSM5102905	S1 T6" >> data/raw/metadata.txt
+echo -e "GSM5511351	NS ES_T0" >> data/raw/metadata.txt
+echo -e "GSM5511352	NS ES_T6" >> data/raw/metadata.txt
+echo -e "GSM5511353	S2_T0" >> data/raw/metadata.txt
+echo -e "GSM5511354	S2_T6" >> data/raw/metadata.txt
+echo -e "GSM5511355	S3_T0" >> data/raw/metadata.txt
+echo -e "GSM5511356	S3_T6" >> data/raw/metadata.txt
+
